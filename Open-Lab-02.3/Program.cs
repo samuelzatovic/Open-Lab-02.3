ab088cc2cd0e7a909edc6663cc256d80141ab8e0
samuel zaťovič <szatovic@gmail.com>
2019-10-13 16:51:30 +0200

    Hotový program

---
+++ Open-Lab-02.3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_02._3
+{
+    class Program
+    {
+        public static int Remainder(int Dividend, int Divisor)
+        {
+            int quotient = Dividend % Divisor;
+            return quotient;
+        }
+    }
+}
